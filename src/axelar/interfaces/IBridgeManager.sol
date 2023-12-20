fd4c56d75afb4710a4b38a982d49b4b132d38d7a
npty <nptytn2@gmail.com>
2023-12-20 13:40:19 +0700

    chore: cleanup naming

--- src/axelar/interfaces/IBridgeManager.sol
+++ src/axelar/interfaces/IBridgeManager.sol
@@ -6,23 +6,23 @@ interface IBridgeManager {
     ///         This function will either lock or burn the sender's tokens.
     ///         If locking - this function will use transferFrom to pull tokens from the user and lock them.
     ///         If burning - this function will call the token's burn function to burn the sender's token.
-    ///         Finally, this function will call into the Bastion contracts to send a message with the incrementing msgId sequence number, msgType = 1, and the token transfer payload.
+    ///         Finally, this function will call into the Endpoint contracts to send a message with the incrementing msgId sequence number, msgType = 1, and the token transfer payload.
     function transfer(
         uint256 amount,
         uint16 recipientChain,
         bytes32 recipient
     ) external returns (uint64 msgId);
 
-    /// @notice Called by a Bastion contract to deliver a verified attestation.
+    /// @notice Called by a Endpoint contract to deliver a verified attestation.
     ///         This function will decode the payload as a BridgeManagerMessage to extract the msgId, msgType, and other parameters.
     ///         When the threshold is reached for a msgId, this function will execute logic to handle the action specified by the msgType and payload.
     function attestationReceived(bytes memory payload) external;
 
-    /// @notice Returns the number of Bastions that must attest to a msgId for it to be considered valid and acted upon.
+    /// @notice Returns the number of Endpoints that must attest to a msgId for it to be considered valid and acted upon.
     function getThreshold() external view returns (uint8);
 
-    /// @notice Returns the Bastion contracts that have been registered via governance.
-    function getBastions() external view returns (address[] memory);
+    /// @notice Returns the Endpoint contracts that have been registered via governance.
+    function getEndpoints() external view returns (address[] memory);
 }
 
 
