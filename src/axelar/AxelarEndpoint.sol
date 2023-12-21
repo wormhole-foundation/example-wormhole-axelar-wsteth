27a50ba7d12ed30fcbcee9bf6b2230e3b16063ab
npty <nptytn2@gmail.com>
2023-12-21 18:36:39 +0700

    chore: remove unnecessary function and update bytes32 to string conversion

--- src/axelar/AxelarEndpoint.sol
+++ src/axelar/AxelarEndpoint.sol
@@ -4,13 +4,14 @@ pragma solidity ^0.8.22;
 import {Ownable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/utils/Ownable.sol";
 import {AxelarExecutable} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/executable/AxelarExecutable.sol";
 import {IAxelarGasService} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/interfaces/IAxelarGasService.sol";
+import {StringToBytes32, Bytes32ToString} from "@axelar-network/axelar-gmp-sdk-solidity/contracts/libs/Bytes32String.sol";
 import {Endpoint} from "./Endpoint.sol";
 import {IBridgeManager} from "./interfaces/IBridgeManager.sol";
 import {EndpointManagerMessage, NativeTokenTransfer} from "./Message.sol";
 
 contract AxelarEndpoint is Endpoint, AxelarExecutable, Ownable {
     IAxelarGasService public immutable gasService;
-    IBridgeManager public bridgeManager;
+    IBridgeManager public endpointManager;
 
     // These mappings are used to convert between chainId and chainName as Axelar accept chainName as string format
     mapping(uint16 => string) public idToAxelarChainIds;
@@ -22,7 +23,7 @@ contract AxelarEndpoint is Endpoint, AxelarExecutable, Ownable {
     ) {
         uint16 chainId = axelarChainIdToId[sourceChain];
         require(
-            emitters[chainId] == keccak256(abi.encodePacked(sourceAddress)),
+            emitters[chainId] == StringToBytes32.toBytes32(sourceAddress),
             "Caller is not the source emitter"
         );
         _;
@@ -35,9 +36,14 @@ contract AxelarEndpoint is Endpoint, AxelarExecutable, Ownable {
         address _owner
     ) AxelarExecutable(_gateway) Ownable(_owner) {
         gasService = IAxelarGasService(_gasService);
-        bridgeManager = IBridgeManager(_bridgeManager);
+        endpointManager = IBridgeManager(_bridgeManager);
     }
 
+    /**
+     * Set the bridge manager contract address
+     * @param chainId The chainId of the chain. This is used to identify the chain in the EndpointManager.
+     * @param chainName The chainName of the chain. This is used to identify the chain in the AxelarGateway.
+     */
     function setAxelarChainId(
         uint16 chainId,
         string calldata chainName
@@ -46,52 +52,47 @@ contract AxelarEndpoint is Endpoint, AxelarExecutable, Ownable {
         axelarChainIdToId[chainName] = chainId;
     }
 
-    function parseEndpointManagerMessage(
-        bytes memory payload
-    ) internal view returns (string memory, bytes memory) {
+    /**
+     * Revert if the message type is not supported
+     */
+    function revertIfInvalidMessageType(bytes memory payload) internal pure {
         // Decode the payload as a BridgeManagerMessage
         EndpointManagerMessage memory message = abi.decode(
             payload,
             (EndpointManagerMessage)
         );
 
-        // Check that the message is a token transfer
-        if (message.msgType == 1) {
-            NativeTokenTransfer memory msgTokenTransfer = abi.decode(
-                message.payload,
-                (NativeTokenTransfer)
-            );
-
-            // Returns destinationChain and destinationContract in a tuple of strings so that compatibles with AxelarGateway's callContract.
-            return (
-                string(abi.encodePacked(emitters[msgTokenTransfer.toChain])),
-                message.payload
-            );
+        // This contract only supports message type 1 which is a NativeTokenTransfer.
+        if (message.msgType != 1) {
+            revert UnsupportedMessageType();
         }
-
-        revert UnsupportedMessageType();
     }
 
+    /**
+     * Send message to Axelar Gateway
+     * @param recipientChain  The chainId of the chain. This is used to identify the chain in the EndpointManager.
+     * @param payload The payload of the message which is a NativeTokenTransfer
+     */
     function _sendMessage(
         uint16 recipientChain,
         bytes memory payload
     ) internal virtual override {
-        (
-            string memory destinationContract,
-            bytes memory _payload
-        ) = parseEndpointManagerMessage(payload);
+        revertIfInvalidMessageType(payload);
 
+        string memory destinationContract = Bytes32ToString.toTrimmedString(
+            emitters[recipientChain]
+        );
         string memory destinationChain = idToAxelarChainIds[recipientChain];
 
         gasService.payNativeGasForContractCall{value: msg.value}(
             address(this),
             destinationChain,
             destinationContract,
-            _payload,
+            payload,
             msg.sender
         );
 
-        gateway.callContract(destinationChain, destinationContract, _payload);
+        gateway.callContract(destinationChain, destinationContract, payload);
     }
 
     /**
@@ -102,7 +103,7 @@ contract AxelarEndpoint is Endpoint, AxelarExecutable, Ownable {
         string calldata sourceAddress,
         bytes calldata payload
     ) internal override onlySourceEmitter(sourceAddress, sourceChain) {
-        bridgeManager.attestationReceived(payload);
+        endpointManager.attestationReceived(payload);
     }
 
     function receiveMessage(
