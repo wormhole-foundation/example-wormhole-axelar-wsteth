18444106ca0bcc1e772349e4eb1837095010af5b
Foivos <foivos@umich.edu>
2024-05-13 15:04:10 +0300

    update Axelar to use the Transceiver Interface

---
+++ src/axelar/interfaces/IAxelarTransceiver.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+import { ITransceiver } from '@wormhole-foundation/native_token_transfer/interfaces/ITransceiver.sol';
+
+interface IAxelarTransceiver is ITransceiver {
+    error InvalidChainId(uint16 chainId);
+
+    event SendTransceiverMessage(uint16 indexed recipientChainId, bytes nttManagerMessage, bytes32 indexed recipientNttManagerAddress, bytes32 indexed refundAddress);
+}
