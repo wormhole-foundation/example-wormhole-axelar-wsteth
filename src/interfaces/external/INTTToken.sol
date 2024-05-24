608da2d025045d6864b85bd6f5112cd9c93262fd
Nikhil Suri <nikhilsuri@comcast.net>
2024-05-23 17:22:40 -0700

    Fix WstEthL2Token

--- src/interfaces/external/INTTToken.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: Apache 2
-pragma solidity ^0.8.19;
-
-interface INTTToken {
-    error CallerNotMinter(address caller);
-
-    function mint(address account, uint256 amount) external;
-}
