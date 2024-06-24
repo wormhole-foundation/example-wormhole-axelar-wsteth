be3491711b80c1211da93089de71f1381ce1024a
Canh Trinh <canhtrinh@users.noreply.github.com>
2024-06-24 15:15:19 -0400

    Update test/axelar/AxelarTransceiver.t.sol
    Co-authored-by: Milap Sheth <milap.sheth2010@gmail.com>

--- test/axelar/AxelarTransceiver.t.sol
+++ test/axelar/AxelarTransceiver.t.sol
@@ -73,6 +73,7 @@ contract AxelarTransceiverTest is Test {
 
         vm.expectEmit(address(transceiver));
         emit AxelarChainIdSet(chainId, chainName, axelarAddress);
+
         vm.prank(OWNER);
         transceiver.setAxelarChainId(chainId, chainName, axelarAddress);
     }
